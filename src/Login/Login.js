3dbbfebf759c596aad83d609edc2311c7a03895c
Akanksha Singh <akanksha.singh@dailmer.com>
2018-11-05 17:14:58 +0530

    css added

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const login = () => {
     return (
-        <div>
+        <div className="text">
             <span>Username</span><input type="text" />
             <span>Password</span><input type="password" />
             <button>Login</button>
